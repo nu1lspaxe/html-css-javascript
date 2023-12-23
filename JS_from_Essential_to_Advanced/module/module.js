3398b354b072d5309296ee8336f953aebc2d2e6e
3nGryCa7 <shrry.ms9395@gmail.com>
2023-12-23 12:29:42 +0800

    Add: Jest

--- JS_from_Essential_to_Advanced/module/module.js
+++
@@ -1,6 +0,0 @@
-const os = require('os');
-
-console.log('platform:', os.platform());
-
-const math = require('./math');
-console.log("original number is %d, after math.double is %d", 5, math.double(5));
---
+++ JS_from_Essential_to_Advanced/module/module.test.js
@@ -0,0 +1,10 @@
+const os = require('os');
+
+// console.log('platform:', os.platform());
+
+const math = require('./math');
+// console.log("original number is %d, after math.double is %d", 5, math.double(5));
+
+test(("[sum] 19999 + 20000"), () => {
+    expect(math.sum(19999, 20000)).toBe(39999);
+})
