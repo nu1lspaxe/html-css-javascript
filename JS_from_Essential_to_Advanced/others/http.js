e618d85a50203f5c1fa55eb1aee1adccafffead9
Nu1LSpaxe <nu1lspaxe.anonym0ux@gmail.com>
2023-12-25 12:52:46 +0800

    upload express.v1

---
+++ JS_from_Essential_to_Advanced/others/http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("I'm http here!");
+})
+    .listen(4000);
+
+console.log("Server running at http://127.0.0.1:4000/");
